31dbf30e9c46527b1fed060ea407b89abe1779fe
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-05-01 11:40:23 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import objectid
+
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = 'myYummyDishes'
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html', recipes=mongo.db.recipes.find())
+
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
