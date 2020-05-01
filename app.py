55c9447a8c0c7b1ca07cfaf4be8a4673c01ff9c0
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-05-01 12:48:22 +0000

    Adding html pages

--- app.py
+++ app.py
@@ -1,20 +1,20 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import objectid
+from bson.objectid import ObjectId
 
 
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = 'myYummyDishes'
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = 'mongodb+srv://root:R00tUser@myfirstcluster-kwp3n.mongodb.net/myYummyDishes?retryWrites=true&w=majority'
 
 mongo = PyMongo(app)
 
 
 @app.route('/')
-@app.route('/recipes')
-def recipes():
+@app.route('/display_recipes')
+def display_recipes():
     return render_template('recipes.html', recipes=mongo.db.recipes.find())
 
 
