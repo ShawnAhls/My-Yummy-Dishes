c61d855979c4af003d8868d13ea83071dfb6e3be
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-05-19 17:17:59 +0000

    got the login and register done just need to stop blank enteries

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def register():
 
         if existing_user is None:
             hashpass = bcrypt.hashpw(request.form['password'].encode('utf-8'), bcrypt.gensalt())
-            users.insert({'name': request.form['username'], 'password' : hashpass})
+            users.insert_one({'name': request.form['username'], 'password': hashpass})
             session['username'] = request.form['username']
             return redirect(url_for('home'))
 
@@ -42,17 +42,19 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         users = mongo.db.users
         login_user = users.find_one({'name': request.form['username']})
 
         if login_user:
-            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password'].encode('utf-8')) == login_user['password'].encode('utf-8'):
+            if bcrypt.hashpw(request.form['password'].encode('utf-8'), login_user['password']) == login_user['password']:
                 session['username'] = request.form['username']
         return redirect(url_for('home'))
+
         flash('Invalid Username/Password combination')
+
     return render_template('login.html')
 
 
@@ -61,7 +63,6 @@ def display_recipes():
     return render_template('display-recipes.html',
                            recipes=mongo.db.recipes.find(),
                            categories=mongo.db.categories.find())
-
 
 
 """
