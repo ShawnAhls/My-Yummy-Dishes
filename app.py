b7558a874cc33da7c514db6ab532d0af81d7deb1
ShawnAhls <shawn.ahlstrom@icloud.com>
2020-05-23 19:41:56 +0000

    Adding env.py file

--- app.py
+++ app.py
@@ -4,11 +4,16 @@ from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
 
+from os import path
+if path.exists("env.py"):
+  import env
+
+
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
 app.config["MONGO_DBNAME"] = 'myYummyDishes'
-app.config["MONGO_URI"] = 'mongodb+srv://root:R00tUser@myfirstcluster-kwp3n.mongodb.net/myYummyDishes?retryWrites=true&w=majority'
+app.config["MONGO_URI"] = 'MONGO_URI'
 
 
 mongo = PyMongo(app)
@@ -16,10 +21,16 @@ mongo = PyMongo(app)
 users = mongo.db.users
 recipes = mongo.db.recipes
 
+mongo.db.recipes.create_index([('$**', 'text')])
+mongo.db.users.create_index([('$**', 'text')])
+
 
 @app.route('/')
 @app.route('/home')
 def home():
+
+    # Loads the home page and indicates which user is logged in
+
     if 'username' in session:
         flash("You are logged in" + session['user'])
     return render_template('home.html',
@@ -29,6 +40,9 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+
+    # Registration for users
+
     if 'user' in session:
         flash('You are already sign in!')
         return redirect(url_for('home'))
@@ -36,10 +50,15 @@ def register():
     if request.method == 'POST':
         form = request.form.to_dict()
 
+        # Check if user exists
+
         if form['password'] == form['password1']:
             user = users.find_one({'name': form['username']})
             if user:
                 flash(f"{form['username']} already exists!")
+
+            # If user does not exist, user can create new account and an encoded password
+
             else:
                 hash_pass = generate_password_hash(form['password'])
                 users.insert_one({
@@ -50,6 +69,9 @@ def register():
             if user:
                 session['user'] = user['name']
                 return redirect(url_for('home'))
+
+    # An error will prompt the user if the password dont match
+
     else:
         """flash("Passwords dont match!")"""
     return render_template("register.html")
@@ -57,6 +79,9 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+
+    # Registered users can login
+
     if "user" in session:
         user = users.find_one({"name": session['user']})
         if user:
@@ -66,13 +91,22 @@ def login():
 
 @app.route('/auth_user', methods=['GET', 'POST'])
 def auth():
+
+    # When user logs in and authenticates the user with the correcct password
+
     form = request.form.to_dict()
     user = users.find_one({"name": form['username']})
     if user:
+
+        # Unhashes the password to check if the correct password was used
+
         if check_password_hash(user['password'], form['password']):
             session['user'] = form['username']
             return redirect(url_for('home'))
         else:
+
+            # If not, an error will appear informing the user
+
             flash('Username/Password are not a match')
             return redirect(url_for('login'))
     else:
@@ -87,6 +121,15 @@ def logout():
     return redirect(url_for('home'))
 
 
+@app.route('/search', methods=["GET", "POST"])
+def search():
+    keywords = request.form.get('search')
+    query = ({'$text': {'$search': keywords}})
+    results = mongo.db.recipes.find(query)
+    return render_template('display-recipes.html',
+                           recipes=results)
+
+
 @app.route('/display_recipes')
 def display_recipes():
     return render_template('display-recipes.html',
@@ -96,16 +139,22 @@ def display_recipes():
 
 @app.route('/add_recipe')
 def add_recipe():
+
+    # Allows the logged in users to add a new recipe
+
     if session:
         return render_template('add-recipe.html',
                                all_categories=mongo.db.categories.find())
     else:
-        flash('You need Sign in first')
+        flash('You need to Sign in first')
     return redirect(url_for('home'))
 
 
 @app.route('/new_recipe', methods=["POST"])
 def new_recipe():
+
+    # Adds a new recipe to the database
+
     new_recipe = {
         'category_name': request.form.get('cartegory_name'),
         'recipe_name': request.form.get('recipe_name'),
@@ -122,6 +171,9 @@ def new_recipe():
 
 @app.route('/edit_recipe/<recipe_id>', methods=["GET", "POST"])
 def edit_recipe(recipe_id):
+
+    # The user can edit their recipe
+
     if session:
         the_recipe = mongo.db.recipes.find({"_id": ObjectId(recipe_id)})
         all_categories = mongo.db.categories.find()
@@ -129,7 +181,7 @@ def edit_recipe(recipe_id):
         return render_template('edit-recipe.html', recipes=the_recipe,
                                categories=category_list)
     else:
-        flash('You need Sign in first')
+        flash('You need to Sign in first')
     return redirect(url_for('home'))
 
 
@@ -162,14 +214,14 @@ def delete(recipe_id):
         flash('Recipe Deleted')
         return render_template('display-recipes.html')
     else:
-        flash('You need Sign in first')
+        flash('You need to Sign in first')
     return redirect(url_for('home'))
 
 
 @app.route('/categories/')
 def categories():
     return render_template('category.html',
-                            categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
 
 
 if __name__ == '__main__':
---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ["MONGO_URI"] = 'mongodb+srv://root:R00tUser@myfirstcluster-kwp3n.mongodb.net/myYummyDishes?retryWrites=true&w=majority'
